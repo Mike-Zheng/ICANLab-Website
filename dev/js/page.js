69464ce8a7f4e66b398401aa1eca066cb167139a
mike <liwenzheng.mike@gmail.com>
2016-05-24 22:16:52 +0800

    member cut

--- dev/js/page.js
+++ dev/js/page.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-    // $('.search').on('click', function() {
-    //     $('.search').css('padding', '10px 50px');
+    // $('.esoe-search').on('click', function() {
+    //     $('.esoe-search').css('padding', '10px 50px');
     // });
 
 
-    $('.search').focus(function() {
-        $('.search').css('padding', '10px 50px');
+    $('.esoe-search').focus(function() {
+        $('.esoe-search').css('padding', '10px 50px');
     });
-    $('.search').blur(function() {
-        $('.search').css('padding', '10px 10px 10px 40px');
+    $('.esoe-search').blur(function() {
+        $('.esoe-search').css('padding', '10px 10px 10px 40px');
     });
+
+$('.right-panel-rwd').on('click', function() {
+    $('.dropdown-rwd').slideToggle();
+});
+
+
 
 });
